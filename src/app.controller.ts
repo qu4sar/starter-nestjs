7f089d6a011c0742386e88784f14688673a21f1e
Salif Omar Faye <salifomar.faye@stride-it.com>
2024-02-25 11:49:02 +0100

    edits

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // -------- TESTING ---------
+  @Get('workspaces')
+  getWorkspaces(): string[] {
+    return ['workspaces', 'workspaces1', 'workspaces2'];
+  }
+
+  @Get('workspaces/:id/socialAccounts')
+  getSocialAccounts(@Body() body: any): string[] {
+    return ['socialAccounts', 'socialAccounts1', 'socialAccounts2'];
   }
 }
